0c81b9804ea63d422da582eb770f871fcc3d8235
Quincy <baininja@hotmail.com>
2016-05-16 00:32:08 -0700

    Test on my own branch.

--- app/src/main/java/com/app/redface/eatwhere/EatWhereMainActivity.java
+++ app/src/main/java/com/app/redface/eatwhere/EatWhereMainActivity.java
@@ -2,6 +2,8 @@ package com.app.redface.eatwhere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class EatWhereMainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class EatWhereMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eat_where_main);
+    }
+
+    public void onBtnClicked(View v) {
+        TextView textViewGaoji = (TextView)findViewById(R.id.textViewGaoji);
+        textViewGaoji.setText("哈哈，搞基啊！");
     }
 }
