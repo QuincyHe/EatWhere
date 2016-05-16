3f1731f94e0600003e3c696ede1d1ac6e3987942
Quincy <baininja@hotmail.com>
2016-05-15 23:59:21 -0700

    Add Project Files.(Use the "Project" view!!!)

---
+++ app/src/main/java/com/app/redface/eatwhere/EatWhereMainActivity.java
@@ -0,0 +1,13 @@
+package com.app.redface.eatwhere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EatWhereMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eat_where_main);
+    }
+}
