4c6b2ea67ec4b96b566b94852780d06a46183342
Quincy <baininja@hotmail.com>
2016-06-16 05:21:39 -0700

    Put in a FragmentTabHost in MainActivity. Added a testing tab page.

--- app/src/main/java/com/app/redface/eatwhere/EatWhereMainActivity.java
+++ app/src/main/java/com/app/redface/eatwhere/EatWhereMainActivity.java
@@ -1,7 +1,11 @@
 package com.app.redface.eatwhere;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class EatWhereMainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class EatWhereMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eat_where_main);
+
+        FragmentTabHost tabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);// Use android.R.id.tabhost for it...
+        tabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        // Add a Tab Page (A Fragment object)
+        tabHost.addTab(tabHost.newTabSpec("tabtest").setIndicator("测试页"), TabPageTestFragment.class, null);
     }
 }
